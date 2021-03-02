4d63576c09dc0cc43c3e019651a467db6cc6eecb
Hugo Gruson <hugo.gruson@protonmail.com>
2021-03-02 17:04:10 +0100

    Translate model and first function in R

---
+++ R/compute_dilution_effect_strength.R
@@ -0,0 +1,81 @@
+#' @param m Number of vector species
+#' @param n Number of reservoir species
+#' @param ... Additional params.
+#'
+#' @example
+#' compute_dilution_effect_strength(
+#'   m = 3, n = 3, N = rep_len(100, 6)
+#' )
+
+compute_dilution_effect_strength <- function(m, n, N, ...) {
+
+  Nr <- seq(100, 10000, length.out = n)
+  Nm <- rep_len(100000, length.out = m)
+  Nvec <- c(Nm, Nr)
+
+  tauM <- rep_len(0.9, length.out = m)
+  tauvec <- c(tauM, runif(n))
+#  tauvec <- c(tauM, seq(0, 1, length.out = n))
+
+  correlation <- cor(tauvec[seq(m+1, m+n)], Nvec[seq(m+1, m+n)])
+
+  b <- c(
+    rep_len(365/30, length.out = m),
+    rep_len(1/4, length.out = n)
+  )
+  d <- b
+
+  sigma <- c(
+    rep_len(0, length.out = m),
+    rep_len(365/7, length.out = n)
+  )
+
+  res <- setNames(
+    data.frame(matrix(NA_real_, ncol = 2, nrow = 10)),
+    c("nspecies", "Imax")
+  )
+
+  for (i in seq_len(10)) {
+
+    N <- c(Nm, Nr[seq(i, n)])
+    tau <- c(tauM, tauvec[seq(m+i, m+n)])
+    ni <- n - i+1
+
+    res$nbspecies[i] <- ni
+
+    phi <- matrix(0, nrow = m+ni, ncol = m+ni)
+
+     for (i in seq_len(m)) {
+       for (j in seq_len(ni)) {
+         phi[i, m+j] <- 365/20
+         phi[m + j, i] <- 365/20
+       }
+     }
+
+    SInit <- N
+    SInit[1] <- N[1]-1
+    names(SInit) <- paste0("S_", seq_along(SInit))
+
+    IInit <- rep_len(0, m+ni)
+    IInit[1] <- 1
+    names(IInit) <- paste0("I_", seq_along(IInit))
+
+    RInit <- rep_len(0, m+ni)
+    names(RInit) <- paste0("R_", seq_along(RInit))
+
+    resi <- deSolve::ode(
+      c(SInit, RInit, IInit),
+      times = seq_len(10),
+      func = run_step_SIR_multihosts,
+      parms = list("tau" = tau, "b" = b, "d" = d, "sigma" = sigma, "phi" = phi, "Nspecies" = m + ni, "alpha" = 0.1)
+    )
+
+    res$Imax[i] <- max(colSums(resi)[grepl("^I_", colnames(resi))])
+
+  }
+
+  slope <- lm(Imax ~ nspecies, data = res)
+
+  return(c(correlation, slope))
+
+}
---
+++ R/run_step_SIR_multihosts.R
@@ -0,0 +1,35 @@
+#' Compute the change of the SIR multihosts system in one time-step
+#'
+#' @param t Current time
+#' @param state Current state of the SEIR system
+#' @param parameters Value of the epidemiological parameters:
+#' - tau Competences
+#' - b Birth rate
+#' - d Death rate
+#' - sigma Infectiosity period
+#' - alpha virulence
+#' - phi Number of contacts
+#'
+#' @return A list with a single element containing the derivative for the
+#' SEIR system, as needed for [deSolve::ode()]
+#'
+#' @export
+run_step_SIR_multihosts <- function(t, state, parameters) {
+
+  with(as.list(parameters), {
+    ### create state matrices
+    S <- matrix(state[grepl("^S_", names(state))], ncol = Nspecies)
+    I <- matrix(state[grepl("^I_", names(state))], ncol = Nspecies)
+    R <- matrix(state[grepl("^R_", names(state))], ncol = Nspecies)
+
+    N <- S + I + R
+
+    lambda <- drop(tau * tcrossprod(phi, I / N))
+
+    dS <- b * N - (lambda + d) * S
+    dI <- lambda * S - (sigma + d + alpha) * I
+    dR <- sigma * I - d * R
+
+    return(list(c(dS, dI, dR)))
+  })
+}
